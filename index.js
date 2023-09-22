e87ed91e218415dd285f9c056a3dd302414ca948
Kossi D. T. Saka <kdtsaka@gmail.com>
2023-09-22 08:10:45 +0200

    Add JSDoc comments for type hints

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+/**
+ * Returns the integer value of any given input value
+ * @param {*} value - Input value
+ * @param {number} [base] - The base
+ * @param {number} [defaultValue] - The default
+ * @returns {number} - The integer value of the
+ */
 const intval = (value, base, defaultValue) => {
     if ("undefined" == typeof value && "undefined" != typeof defaultValue) {
         return defaultValue;
